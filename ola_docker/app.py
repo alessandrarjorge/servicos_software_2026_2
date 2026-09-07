e8fc363ff8f8ed25c51e00552ffa16573f2d8126
Alessandra <alessandrarjorge@gmail.com>
2026-09-07 16:04:09 -0300

    Aleteração de mensagem do app

--- ola_docker/app.py
+++ ola_docker/app.py
@@ -14,7 +14,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return 'Hello World (Python) Executando por %s' % elapsed()
+    return 'Olá mundo! (Python) Executando por %s' % elapsed()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
